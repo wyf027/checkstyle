ac8789d04bfc59e233c4af906971896387b1acb5
wuyangfan <1102042793@qq.com>
2026-06-01 06:08:19 +0800

    Issue #19764: Move violation comments out of Javadoc for InputEmptyLineSeparatorJavadocCommentAfterPackage

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorJavadocCommentAfterPackage.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/whitespace/emptylineseparator/InputEmptyLineSeparatorJavadocCommentAfterPackage.java
@@ -1,5 +1,5 @@
-package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator;
-/** // violation ''/\*' should be separated from previous line.'
+package com.puppycrawl.tools.checkstyle.checks.whitespace.emptylineseparator; // violation 2 lines below ''/\*' should be separated from previous line.'
+/**
  * Some javadoc here.
  */
 
